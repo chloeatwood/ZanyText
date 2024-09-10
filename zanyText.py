9baf8e587dfad6be270904b828834db28da459ba
chloeatwood <catwood0419@gmail.com>
2024-09-10 10:17:45 -0500

    Adding game

---
+++ zanyText.py
@@ -0,0 +1,158 @@
+# <Chloe Atwood>             <1/23/2024>
+# <Assignment 1>
+# <Writing Zany Texts to practice using the print and input cammands while also practicing writing things in script>
+
+# TODO: Student writes the assignment number, and a brief description of what the assignment is inside the comment block above.
+#       This description should document what the code is and what it does, such that a new user can read it and understand what
+#       the script does.
+#       (delete this TODO line when completed) (1 pt.)
+
+# TODO: Student turns in their assignment before the 11:59 p.m. Friday deadline and the file is named zanyText.py
+#       (delete this TODO line when completed) (1 pt.)
+
+# Parts of Speech:
+# CITATION: http://www.butte.edu/departments/cas/tipsheets/grammar/parts_of_speech.html
+# ACCESSED: 1-22-2023
+#
+# NOUN - "A noun is the name of a person, place, thing, or idea." 
+#      - "man... Butte College... house... happiness"
+# PRONOUN - "A pronoun is a word used in place of a noun." 
+#         - "She... we... they... it"
+# VERB - "A verb expresses action or being." 
+#      - "jump... is... write... become"
+# ADJECTIVE - "An adjective modifies or describes a noun or pronoun." 
+#           - "pretty... old... blue... smart"
+# ADVERB - "An adverb modifies or describes a verb, an adjective, or another adverb." 
+#        - "gently... extremely... carefully... well"
+# PREPOSITION - "A preposition is a word placed before a noun or pronoun to form a phrase modifying another word in the sentence." 
+#             - "by... with.... about... until"
+# CONJUNCTION - "A conjunction joins words, phrases, or clauses." 
+#             - "and... but... or... while... because"
+# INTERJECTION - "An interjection is a word used to express emotion." 
+#              - "Oh!... Wow!... Oops!"
+
+print("Zany Text!")
+print()
+
+
+print("By: <Chloe Atwood>")
+print("[COM S 127 <A>]")
+print()
+
+# 'Zany Text' #1
+print("Zany Text #1")
+print()
+
+# Gathering input
+noun1 = input("noun: ")
+adjective1 = input("adjective: ")
+adjective2 = input("adjective: ")
+adjective3 = input("adjective: ")
+verb1 = input("past tense verb: ")
+noun2 = input("noun: ")
+print() # Print a blank line
+
+# Printing the final string
+print("Once upon a time, there was a " + 
+        noun1 + 
+        ". It was a " +
+        adjective1 + ", " + 
+        adjective2 + ", " + 
+        adjective3 + " " +
+        noun1 + 
+        ". And, one day it " + 
+        verb1 + " " +
+        "all over the " + 
+        noun2 + "!")
+
+# Print a blank line between Zany Texts
+print()
+
+# TODO: Make three additional 'Zany Texts' like the one above, and have all four run/ print out in order. 
+#       There should be exactly four (4) 'Zany Texts' in the completed assignment.
+#       (delete this TODO line when completed) (2 pts each => 6 pts. total)
+
+# 'Zany Text' #2 (2 pts.)
+print("Zany Text #2")
+print()
+
+print("By: <Chloe Atwood>")
+print("[COM S 127 <A>]")
+print()
+
+# Gathering input
+food = input('Food:')
+numberB1 = input('Number:')
+nounB1 = input('Noun:')
+nounB2 = input('Noun:')
+nounB3 = input('Noun:')
+verbB1 = input('Verb:')
+numberB2 = input('Number:')
+numberB3 = input('Number:')
+print()
+
+
+# Printing the final string
+print(' Have you ever wanted to know how to make ' + food + ' ? Well first you add ' + numberB1 + ' cups of ' + nounB1 
+      + ', then you add 2 large ' + nounB2 + ', and 2.25 cups of ' + nounB3 +'. Once that is complete you must grab your whisk and ' + verbB1 
+      + ' all the ingredients together. After that all that is left is to measure out ' + numberB2 + ' tablespoons and place on a hot pan cooking on medium low heat. Wait ' 
+      + numberB3 + ' minutes then flip. Repeat this last step waiting ' + numberB3 + ' minutes then remove from pan and enjoy!')
+
+# Print a blank line between Zany Texts
+print()
+
+# 'Zany Text' #3 (2 pts.)
+print("Zany Text #3")
+print()
+
+print("By: <Chloe Atwood>")
+print("[COM S 127 <A>]")
+print()
+
+# Gathering input
+mammal = input('Mammal:')
+name = input('Name:')
+activity = input('Present tense activity:')
+verbC1 = input('Verb ending in ing:')
+location1 = input('Location:')
+location2 = input('Location:')
+print()
+
+# Printing the final string
+print('Once upon a time there was a ' + mammal + ' named ' + name + '. One day ' + name + ' decided to take a vacation to ' + location1 + '. Once ' 
+      + name + ' the ' + mammal + ' arrived, they decided to go outside and give ' + activity + ' a try. They went outside and did this for a little while but soon got homesick. '
+      + name + ' was ' + verbC1 + ' all over the place as a result. ' + name + ' ended up cutting the visit short and returning to ' + location2 + '.')
+
+# Print a blank line between Zany Texts
+print()
+
+# 'Zany Text' #4 (2 pts.)
+print("Zany Text #4")
+print()
+
+print("By: <Chloe Atwood>")
+print("[COM S 127 <A>]")
+print()
+
+# Gathering input
+numberD1 = input('Number:')
+nameD1 = input('Boys Name:')
+adjectiveD1 = input('Adjective:')
+nounD1 = input('Noun:')
+timeD1 = input('Number:')
+verbD1 = input('Verb:')
+nameD2 = input('Supervillan Name:')
+print()
+
+# Printing the final string
+print('A long time ago in a galaxy far far away, there lived ' + numberD1 + 
+      ' jedi on  the planet Coruscant. One day two jedi (a master nad his apprentice) stumbled upon a child named ' 
+      + nameD1 + '. They soon realized this young child had more potential jedi power than any other to ever exist. The child however was more concerned with his mother and '
+      'a woman traveling with them. This child thought the woman looked like the most ' + adjectiveD1 + ' woman to ever exist. The master jedi decided he was going to bring this child'
+      ' back with him to Coruscant and train him to be a jedi. This child was soon corrupted when an evil ' + nounD1 + ' decided to pose as a good leader turn this child '
+      ' against everything he would grow to love. After about ' + timeD1 + ' years, this child grew up and used his evil to violently ' 
+      + verbD1 + ' everyone located in the training temple on Coruscant. ' + nameD1 + ' then got into a death match with the man who raised him.'
+      ' This resulted in ' + nameD1 + ' turning into the evil lord ' + nameD2 + ' and taking over the galaxy.')
+
+# Print a blank line between Zany Texts
+print()
